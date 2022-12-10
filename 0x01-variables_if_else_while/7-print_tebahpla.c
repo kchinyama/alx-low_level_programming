fa7d2e3bb854a82889c4b451a58ce9fbc4b627c0
Kaumba Chinyama <kaumba@kalichipictures.com>
2022-12-10 09:58:03 -0800

    7 print alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: (0) always success
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'z';
+	while
+		(k >= 'a') {
+			putchar(k);
+			k--;
+		}
+	putchar('\n');
+	return (0);
+}
