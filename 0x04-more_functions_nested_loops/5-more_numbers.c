f54d25fdfc9a03cf4ae9bcbe52efadf01867fb79
Kaumba Chinyama <kaumba@kalichipictures.com>
2022-12-16 08:26:22 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more numbers - prints numbers to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int number1, number2;
+
+	for (number1 = 0; number1 < 10; number++)
+	{
+	for (number2 = 0; number2 <= 14; number2++)
+	{
+		if (number2 > 9)
+		{
+			_putchar((number2 / 10) + '0');
+		}
+			_putchar((number2 % 10) + '0');
+	}
+	_putchar(10);
+	}
+}
