36f9938b7af63a3babe6e5b9732c3ef9f6117101
Kaumba Chinyama <kaumba@kalichipictures.com>
2022-12-16 08:14:31 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * function that prints numbers 0 to 9, excludes 2 and 4 then new line
+ *
+ * Return result
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+	for (number = 48; number < 58; number++)
+	{
+	if ((number == 50) || (number == 52))
+	{
+		continue;
+	}
+	_putchar(number);
+	}
+	_putchar(10)
+}
