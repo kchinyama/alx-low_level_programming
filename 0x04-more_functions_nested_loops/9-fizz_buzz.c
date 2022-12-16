2b1dac4b726071f1236158364434c84ec7ae9f23
Kaumba Chinyama <kaumba@kalichipictures.com>
2022-12-16 12:49:29 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ * 
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int k = 100;
+	int i = 1;
+
+	while (i <= k)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0);
+	{
+	printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+	if (i < k)
+	printf("Buzz ");
+	else
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%i ", i);
+	}
+	i++;
+
+	}
+	printf("\n");
+
+	return (0);
+}
