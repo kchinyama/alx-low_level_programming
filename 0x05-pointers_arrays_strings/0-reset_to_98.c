94b6eb3e66f3c48141abdc0d79f1adc4d6a07f54
Kaumba Chinyama <kaumba@kalichipictures.com>
2022-12-19 11:12:05 -0800

    main header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function that takes pointer to int and updates value to 98
+ *
+ * Return: 0 always
+ */
+
+void reset_to_98(int *n)
+{
+	int var = 98;
+	int *n;
+
+	ip = &var;
+	_putchar("Value of *n variable: %d\n", *ip);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains completed tasks under the 0x05. C - Pointers, arrays and strings Project
