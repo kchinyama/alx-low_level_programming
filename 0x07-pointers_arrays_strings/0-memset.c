38ed4df5d0f5ea798cc4cb4d6c159f9856e2b077
Kaumba Chinyama <kaumba@kalichipictures.com>
2023-01-02 10:56:58 -0800

    main and README files upload

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset- function fills memory of n bytes with b
+ * @n: memory bytes
+ * @s: pointer
+ * @b: filled bytes
+ * Return: pointer to memory of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains completed tasks under the '0x07. C - Even more pointers, arrays and strings' Project(s)
