ee41d67c958ac39bdd5897b4c3533d5784b78574
Kaumba Chinyama <kaumba@kalichipictures.com>
2023-01-07 13:34:57 -0800

    Task 0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function is our entry point
+ * @argc: int value showing number of commands in command line
+ * @argv: pointer to string
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int k;
+
+	printf("argc = %d\n", argc);
+	printf("My name is:\n");
+
+	for (k = 0; k < argc; k++)
+	{
+		printf("argv[%d] = %s\n", k, argv[k]);
+	}
+	return (0);
+}
