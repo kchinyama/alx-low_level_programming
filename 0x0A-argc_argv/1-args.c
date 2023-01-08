bef09afce348c100285d37d772288e510021d426
Kaumba Chinyama <kaumba@kalichipictures.com>
2023-01-08 10:41:12 -0800

    Task 1. 'Silence is argument carried out by other means'

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - our entry point
+ * @argc: points to number of arguments
+ * @argv: points to number strings in arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
