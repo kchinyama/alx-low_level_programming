e698d7babd1016dc67f983e2e43a0c8da35683ff
Kaumba Chinyama <kaumba@kalichipictures.com>
2023-01-17 05:58:30 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef STRTYP
+#define STRTYP
+/**
+ * struct dog - is struct name
+ * @name: pointer to char
+ * @age: pointer to float value
+ * @owner: pointer to owner
+ * Return: nothing
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+	 * dog_t - typedef for struct dog
+	 */
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+#endif
