136912776fb58b632245136eda2ceb0ef08fae0f
kchinyama <kaumba@kalichipictures.com>
2023-02-01 07:41:41 -0800

    task 9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (k = 0; temp && k < idx; k++)
 	{
 	if (k == idx - 1)
-	}
+	{
 	newNode->next = temp->next;
 	temp->next = newNode;
 	return (newNode);
+	}
+	else
+		temp = temp->next;
+	}
+	return (NULL);
 }
+
