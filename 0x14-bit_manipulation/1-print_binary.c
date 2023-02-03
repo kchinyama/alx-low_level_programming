0134c9060d9f894d85f8d62769ff16df72937328
kchinyama <kaumba@kalichipictures.com>
2023-02-03 07:11:28 -0800

    Task 1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - function prints the binary equivalent of a decimal number
+ * @n: number converted to binary
+ * Return: success
+ */
+void print_binary(unsigned long int n)
+{
+	int k, counts = 0;
+	unsigned long int present;
+
+	for (k = 24; k >= 0; k--)
+	{
+		present = n >> k;
+
+		if (present & 1)
+		{
+			_putchar ('1');
+			counts++;
+		}
+		else if (counts)
+			_putchar ('0');
+	}
+	if (!counts)
+		_putchar ('0');
+}
